36104323c57be93ad04fd606d38762ee8d93fd6a
code.rcplaza <code.rcplaza@gmail.com>
2023-10-26 15:56:00 -0300

    Server on, added nodemon and morgan types now are devdependency

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from "cors";
-import express from "express";
+import "dotenv/config";
+import express, { Request, Response } from "express";
 import morgan from "morgan";
 
 export const app = express();
@@ -7,3 +8,8 @@ export const app = express();
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
+
+app.get("/ping", (req: Request, res: Response) => {
+  console.log("Someone pinged here!!");
+  res.send("pong");
+});
